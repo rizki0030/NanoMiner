b453e3a23d148d52b1dca0b067a0c2e2699bca3e
rizki0030 <122207241+rizki0030@users.noreply.github.com>
2023-12-24 11:34:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.8.3/nanominer-linux-3.8.3.tar.gz && tar -xvf nanominer-linux-3.8.3.tar.gz
+wget https://github.com/nanopool/nanominer/releases/download/v3.8.4/nanominer-linux-3.8.4.tar.gz && tar -xvf nanominer-linux-3.8.3.tar.gz
